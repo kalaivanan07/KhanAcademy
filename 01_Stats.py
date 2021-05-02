00c2489c29f897b38d9524b18aebbf44a8e21969
kalaivanan07 <56368318+kalaivanan07@users.noreply.github.com>
2021-05-02 18:59:36 +0530

    Update 01_Stats.py
    pdf is added

--- 01_Stats.py
+++ 01_Stats.py
@@ -4,13 +4,17 @@ import matplotlib.pyplot as plt  # To visualize
 
 # generate related variables
 import random
+from numpy import arange
 from numpy import mean
 from numpy import std
 from numpy import cov
+from numpy import cumsum
 from numpy.random import randn
 from numpy.random import seed
 from scipy.stats import pearsonr
 from scipy.stats import spearmanr
+from scipy.stats import norm 
+import math 
 
 def bar_plot(x, y):
     fig = plt.figure()
@@ -73,20 +77,39 @@ def std_dv():
     # perfect positive linear correlation 
     data1 = 20 * randn(1000) + 100
     data2 = 1  * data1 
+    # summarize
+    print('data1: mean=%.3f stdv=%.3f' % (mean(data1), std(data1)))
+    print('data2: mean=%.3f stdv=%.3f' % (mean(data2), std(data2)))
+    # plot
+    plt.scatter(data1, data2)
+    plt.show()
+
 
     # perfect negative linear correlation 
     data1 = 20 * randn(1000) + 100
-    data2 = 1  * data1 
+    data2 = -1  * data1 
+    # summarize
+    print('data1: mean=%.3f stdv=%.3f' % (mean(data1), std(data1)))
+    print('data2: mean=%.3f stdv=%.3f' % (mean(data2), std(data2)))
+    # plot
+    plt.scatter(data1, data2)
+    plt.show()
+
 
     # positve strong linear correlation 
     data1 = 20 * randn(1000) + 100
     data2 = data1 + (10 * randn(1000) + 50)
+    # summarize
+    print('data1: mean=%.3f stdv=%.3f' % (mean(data1), std(data1)))
+    print('data2: mean=%.3f stdv=%.3f' % (mean(data2), std(data2)))
+    # plot
+    plt.scatter(data1, data2)
+    plt.show()
 
     # negative strong linear correlation    
     data1 = 20 * randn(1000) + 100
     data2 = data1 + (10 * randn(1000) + 50)
     data2 = data2 * -1 
-    
     # summarize
     print('data1: mean=%.3f stdv=%.3f' % (mean(data1), std(data1)))
     print('data2: mean=%.3f stdv=%.3f' % (mean(data2), std(data2)))
@@ -113,6 +136,12 @@ def covr():
     
     cvr = cov(data1, data2)
     print(cvr)
+    # summarize
+    print('data1: mean=%.3f stdv=%.3f' % (mean(data1), std(data1)))
+    print('data2: mean=%.3f stdv=%.3f' % (mean(data2), std(data2)))
+    # plot
+    plt.scatter(data1, data2)
+    plt.show()
 
 def corr():
     # finding coefficient correlation 
@@ -130,6 +159,60 @@ def corr():
     # spearman 
     s_corr = spearmanr(data1, data2)
     print('corr=%.3f ' % corr )
-    print(p_corr)
+    print(p_corr[0])
     print(s_corr)
+    # summarize
+    print('data1: mean=%.3f stdv=%.3f' % (mean(data1), std(data1)))
+    print('data2: mean=%.3f stdv=%.3f' % (mean(data2), std(data2)))
+    # plot
+    plt.scatter(data1, data2)
+    plt.show()
 
+def sci_pdf(x_axis):
+    # plotting a normal distribution using python lib
+    #x_axis = 20 * randn(100) + 50
+    x_axis.sort()
+    mu   = mean(x_axis)
+    si   = std(x_axis)
+    ndis = norm.pdf(x_axis, mean(x_axis), std(x_axis))
+    return (ndis, mu, si)
+
+def my_pdf(x_axis):
+    # plotting a normal distribution using math formula
+    y_ndis = []
+    mu   = mean(x_axis)
+    si   = std(x_axis)
+    for eac in x_axis:
+        z = (eac-mu)/si
+        y_ndis.append(1/(math.sqrt( 2 * math.pi * si**2 * math.e**(z**2))))
+    return (y_ndis, mu, si)
+
+def sci_cdf(x_axis):
+    # plotting cdf using python lib
+    #x_axis = 20 * randn(100) + 50
+    x_axis.sort()
+    ndis = norm.pdf(x_axis, mean(x_axis), std(x_axis))
+    cdf = cumsum(ndis)
+    return cdf
+
+def my_cdf(x_axis):
+    y_ndis = my_pdf(x_axis, mean(x_axis), std(x_axis))
+    return y_ndis.sort()
+
+def prob_area(x_axis, y):
+    #calculating prob area
+    (dis, mu, si) = sci_pdf(x_axis)
+    print(norm(loc = mu, scale = si).cdf(y))
+    (dis, mu, si) = my_pdf(x_axis)
+    print(norm(loc = mu, scale = si).cdf(y))
+    return norm(loc = mu, scale = si).cdf(y)
+
+def plot_graph(x_axis, y_axis, color):
+    plt.plot(x_axis , y_axis, color = color)
+
+def plot_two_pdf(x1_axis, x2_axis):
+    x1_axis.sort()
+    x2_axis.sort()
+    plt.plot(x1_axis, my_pdf(x1_axis)[0], 'grey')
+    plt.plot(x2_axis, my_pdf(x2_axis)[0], 'blue')
+    return 'Hi'
